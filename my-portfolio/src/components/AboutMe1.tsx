4f2595cf7e5cbce21270f577b0769f49de263150
haldanek <khaldane244@gmail.com>
2025-06-16 16:24:58 -0400

    chore: made image cover width of container

--- my-portfolio/src/components/AboutMe1.tsx
+++ my-portfolio/src/components/AboutMe1.tsx
@@ -1,23 +1,22 @@
 /* eslint-disable @next/next/no-img-element */
-import { Card, CardHeader } from '@/components/ui/card'
-
-
+import { Card, CardHeader } from '@/components/ui/card';
 
 const AboutMe = () => {
   return (
-    <section id="logo" className="scroll-mt-16">
-      <Card className="comic-card">
-        <CardHeader>
-        </CardHeader>
-       
-         <div>
-          <img src="/images/kplogo.png" alt="Logo" />
-         </div>
-       
+    <section id='logo' className='scroll-mt-16'>
+      <Card className='comic-card'>
+        <CardHeader></CardHeader>
+
+        <div className='w-full h-full'>
+          <img
+            src='/images/kplogo.png'
+            alt='Logo'
+            className='w-full h-full object-cover'
+          />
+        </div>
       </Card>
     </section>
-  )
-}
+  );
+};
 
-export default AboutMe
-
+export default AboutMe;
