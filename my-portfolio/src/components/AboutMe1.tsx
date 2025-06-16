e48b04b0dede0e713b46c234754f60b0a1bd773b
haldanek <khaldane244@gmail.com>
2025-06-16 16:51:26 -0400

    chore: changed size and border of logo container

--- my-portfolio/src/components/AboutMe1.tsx
+++ my-portfolio/src/components/AboutMe1.tsx
@@ -1,28 +1,24 @@
 /* eslint-disable @next/next/no-img-element */
-import {
-  Card,
-  CardHeader,
-  CardContent,
-} from '@/components/ui/card'
+import { Card, CardHeader, CardContent } from '@/components/ui/card';
 
 const AboutMe = () => {
   return (
-    <section id="logo" className="scroll-mt-0 p-0 m-0">
-      <Card className="comic-card p-0 m-0 boarder-none shadow-none rounded-none">
-        <CardHeader className="p-0 m-0" />
+    <section id='logo' className='scroll-mt-0 p-0 m-0'>
+      <Card className='comic-card  rounded-xl overflow-hidden p-0 m-0 boarder shadow'>
+        <CardHeader className='p-0 m-0' />
 
-        <CardContent className="p-0 m-0">
-          <div className="w-full h-[300px]">
+        <CardContent className='p-0 m-0'>
+          <div className='w-full h-[900px]'>
             <img
-              src="/images/kplogo.png"
-              alt="Logo"
-              className="w-full h-full object-cover block"
+              src='/images/kplogo.png'
+              alt='Logo'
+              className='w-full h-full object-cover block'
             />
-          </div>  
+          </div>
         </CardContent>
       </Card>
     </section>
-  )
-}
-  
-  export default AboutMe
+  );
+};
+
+export default AboutMe;
