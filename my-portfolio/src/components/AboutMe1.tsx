3a0d9924c58ffcd570fc22c640fabe61639d4d59
haldanek <khaldane244@gmail.com>
2025-06-16 16:46:31 -0400

    fix: remove padding from logo container

--- my-portfolio/src/components/AboutMe1.tsx
+++ my-portfolio/src/components/AboutMe1.tsx
@@ -1,22 +1,28 @@
 /* eslint-disable @next/next/no-img-element */
-import { Card, CardHeader } from '@/components/ui/card';
+import {
+  Card,
+  CardHeader,
+  CardContent,
+} from '@/components/ui/card'
 
 const AboutMe = () => {
   return (
-    <section id='logo' className='scroll-mt-16'>
-      <Card className='comic-card'>
-        <CardHeader></CardHeader>
+    <section id="logo" className="scroll-mt-0 p-0 m-0">
+      <Card className="comic-card p-0 m-0 boarder-none shadow-none rounded-none">
+        <CardHeader className="p-0 m-0" />
 
-        <div className='w-full h-[300px]'>
-          <img
-            src='/images/kplogo.png'
-            alt='Logo'
-            className='w-full h-full object-cover'
-          />
-        </div>
+        <CardContent className="p-0 m-0">
+          <div className="w-full h-[300px]">
+            <img
+              src="/images/kplogo.png"
+              alt="Logo"
+              className="w-full h-full object-cover block"
+            />
+          </div>  
+        </CardContent>
       </Card>
     </section>
-  );
-};
-
-export default AboutMe;
+  )
+}
+  
+  export default AboutMe
